f5f4f5e9e3010139987655147616bc0830ec7753
tputerio <47957367+tputerio@users.noreply.github.com>
2025-07-04 15:22:08 +0000

    Add build process and optimized CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.{html,js}', // This scans your html and js files for classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
